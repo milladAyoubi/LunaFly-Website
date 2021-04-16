ac1490f4ca0fde24e0928689034ab8f6a5868f91
Millad Ayoubi <Milladayoubi@gmail.com>
2021-04-16 18:41:28 -0400

    Interactive Hover Animation

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -4,13 +4,31 @@ var butt = document.querySelector("#buttonBook");
 var mainImage = document.querySelector(".mainImage")
 var mainText = document.querySelector(".mainText")
 
-
-
-
 var planetTab = document.querySelectorAll(".planet")
 
 
 
+function loadContent() {
+
+    var destinationHeight = document.querySelector(".Destinations")
+    var bodyHeight = document.querySelector('body')
+    var y = bodyHeight.scrollTop;
+    var y1 = destinationHeight.getBoundingClientRect()
+    console.log(y)
+    console.log(y1.top)
+
+    if (y1.top <= 235) {
+        planetTab.forEach((e, i) => {
+            setTimeout(function() {
+                e.classList.add("FadeTop")
+
+
+            }, i * 300);
+        })
+
+    }
+
+}
 const onLoad = () => {
 
     setTimeout(function() {
@@ -20,19 +38,12 @@ const onLoad = () => {
 
     mainText.classList.add("FadeLeft")
 
-
-
-
 }
 
 
+
+
+
+
+
 onLoad()
-
-
-planetTab.forEach((e, i) => {
-    setTimeout(function() {
-        e.classList.add("FadeTop")
-
-
-    }, i * 300);
-})
